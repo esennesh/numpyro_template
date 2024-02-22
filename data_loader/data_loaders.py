c932aba22e47d1adbb4a3052e5d4656bf11bc179
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-02-21 21:22:29 -0600

    data_loaders.IndexedDataset: wrap a Dataset and return indices with data
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -9,13 +9,24 @@ class FlattenAndCast(object):
     def __call__(self, pic):
         return np.ravel(np.array(pic, dtype=jnp.float32))
 
+class IndexedDataset(Dataset):
+    def __init__(self, dataset):
+        self._dataset = dataset
+
+    def __getitem__(self, idx):
+        (data, target) = self._dataset[idx]
+        return (data, target, np.array([idx]))
+
+    def __len__(self):
+        return len(self._dataset)
+
 class MnistDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=FlattenAndCast())
+        self.dataset = IndexedDataset(datasets.MNIST(self.data_dir, train=training, download=True, transform=FlattenAndCast()))
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 @jit
