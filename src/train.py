b9adffd55b4315b05871e07c99d56ab489a47bf9
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-06-12 17:19:08 -0500

    train: enable testing at the end of training
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- src/train.py
+++ src/train.py
@@ -59,20 +59,18 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
 
     if cfg.get("train"):
         log.info("Starting training!")
-        # trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
         trainer.train(monad, datamodule, ckpt_path=cfg.get("ckpt_path"))
 
     train_metrics = trainer.train_metrics.result()
 
     if cfg.get("test"):
         log.info("Starting testing!")
-        # ckpt_path = trainer.checkpoint_callback.best_model_path
-        # if ckpt_path == "":
-        #     log.warning("Best ckpt not found! Using current weights for testing...")
-        #     ckpt_path = None
-        # trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
-        test_metrics = trainer.test(monad, datamodule, ckpt_path=None)
-        # log.info(f"Best ckpt path: {ckpt_path}")
+        ckpt_path = str(trainer.checkpoint_dir + '/model_best')
+        if ckpt_path == "" or not os.path.exists(ckpt_path):
+            log.warning("Best ckpt not found! Using current weights for testing...")
+            ckpt_path = None
+        test_metrics = trainer.test(monad, datamodule, ckpt_path=ckpt_path)
+        log.info(f"Best ckpt path: {ckpt_path}")
     else:
         test_metrics = {}
 
